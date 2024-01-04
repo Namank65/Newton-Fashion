01fe9a1d8771ef90d20a119686b48acab4d7359f
Naman <namankumar62039@gmail.com>
2024-01-05 00:52:35 +0530

    worked on navbar css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
-import logo from "../Assets/logo.png";
+import logo from "../Assets/ecomlogo2.png";
 import cart_icon from "../Assets/cart_icon.png";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
   return (
     <div className="navbar">
-      <div className="logoImg">
-        <img src={logo} alt="Logo" />
+      <div className="logoContainer">
+        <img className="logoimg" src={logo} alt="Logo" />
+        <p>NuBî Fashîon</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop</li>
+        <li>
+          Shop <hr />
+        </li>
         <li>Men</li>
         <li>Women</li>
         <li>Kids</li>
@@ -18,6 +22,7 @@ const Navbar = () => {
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt="CartIcon" />
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   );
