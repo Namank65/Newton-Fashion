e9acd3ac623dde8389fc51bebc50dd4a110abcd0
Naman <namankumar62039@gmail.com>
2024-01-12 00:44:58 +0530

    created context api for the app

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './Footer.css';
 import footer_logo from "../Assets/ecomlogo2.png";
 import instagram_icon from '../Assets/instagram_icon.png';
+import pintester_icon from '../Assets/pintester_icon.png';
+import whatsapp_icon from '../Assets/whatsapp_icon.png';
 
 const Footer = () => {
   return (
@@ -19,8 +21,18 @@ const Footer = () => {
         </ul>
         <div className="footer-social-icon">
             <div className="footer-social-container">
-                <img src={instagram_icon} alt="" />
+                <img src={instagram_icon} alt="instagram-icon" />
             </div>
+            <div className="footer-social-container">
+                <img src={pintester_icon} alt="pintester-icon" />
+            </div>
+            <div className="footer-social-container">
+                <img src={whatsapp_icon} alt="whatsApp-icon" />
+            </div>
+        </div>
+        <div className="footer-copyright">
+            <hr />
+            <p>Copyright @ 2024 All Right Reserved</p>
         </div>
     </div>
   )
