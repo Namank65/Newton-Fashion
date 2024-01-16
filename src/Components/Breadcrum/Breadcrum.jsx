a2c8f06a1635f04394cb8fd90fdb0b135da3052f
Naman <namankumar62039@gmail.com>
2024-01-17 00:16:39 +0530

    added breadcrum component to the product page

--- src/Components/Breadcrum/Breadcrum.jsx
+++ src/Components/Breadcrum/Breadcrum.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import './Breadcrum.css';
+import React from "react";
+import "./Breadcrum.css";
+import arrow_icon from "../Assets/breadcrum_arrow.png";
 
-const Breadcrum = () => {
+const Breadcrum = (props) => {
+  const { product } = props;
   return (
-    <div className='breadcrum'>
-
+    <div className="breadcrum">
+      Home <img src={arrow_icon} alt="arrowIcon" /> Shop
+      <img src={arrow_icon} alt="arrowIcon" /> {product.category}
+      <img src={arrow_icon} alt="arrowIcon" />{product.name}
     </div>
-  )
-}
+  );
+};
 
 export default Breadcrum;
