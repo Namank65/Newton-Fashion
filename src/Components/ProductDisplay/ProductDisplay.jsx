75a3ac0ae2ef126bb41bdff39ce8bb366ffa3a34
Naman <namankumar62039@gmail.com>
2024-01-17 23:52:31 +0530

    created productDisplay component and productDisplay css file

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,41 +1,70 @@
-import React from 'react';
-import './ProductDisplay.css';
-import star_icon from '../Assets/star_icon.png';
-import star_dull_icon from '../Assets/star_dull_icon.png';
+import React from "react";
+import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
 
 const ProductDisplay = (props) => {
-
-    const {product} = props;
+  const { product } = props;
   return (
-    <div className='ProductDisplay'>
-        <div className="ProductDisplay-left">
-            <div className="ProductDisplay-img-list">
-                <img src={product.image} alt="" />
-                <img src={product.image} alt="" />
-                <img src={product.image} alt="" />
-                <img src={product.image} alt="" />
-            </div>
-            <div className="ProductDisplay-img">
-                <img className='ProductDisplay-main-img' src={product.image} alt="productMainImg" />
-            </div>
+    <div className="ProductDisplay">
+      <div className="ProductDisplay-left">
+        <div className="ProductDisplay-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
         </div>
-        <div className="ProductDisplay-right">
-            <h1>{product.name}</h1>
-            <div className="ProductDisplay-right-star">
-                <img src={star_icon} alt="" />
-                <img src={star_icon} alt="" />
-                <img src={star_icon} alt="" />
-                <img src={star_icon} alt="" />
-                <img src={star_dull_icon} alt="" />
-                <p>(122)</p>
-            </div>
-            <div className="ProductDisplay-right-prices">
-                
-            </div>
-
+        <div className="ProductDisplay-img">
+          <img
+            className="ProductDisplay-main-img"
+            src={product.image}
+            alt="productMainImg"
+          />
         </div>
+      </div>
+      <div className="ProductDisplay-right">
+        <h1>{product.name}</h1>
+        <div className="ProductDisplay-right-star">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+          <p>(122)</p>
+        </div>
+        <div className="ProductDisplay-right-prices">
+          <div className="ProductDisplay-right-price-old">
+            ${product.old_price}
+          </div>
+          <div className="ProductDisplay-right-price-new">
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="ProductDisplay-right-description">
+          A lightweight, usually knitted, pullover shirt, close fitting and well
+          rounded neckline and short sleeves, worn as an undershirt or outer
+          garment.
+        </div>
+        <div className="ProductDisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="ProductDisplay-right-size">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button>Add To Cart</button>
+        <p className="ProductDisplay-right-category">
+          <span>Category :</span>Women, T-Shirt, Crop-top
+        </p>
+        <p className="ProductDisplay-right-category">
+          <span>Tags :</span>Modern, Latest
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProductDisplay;
