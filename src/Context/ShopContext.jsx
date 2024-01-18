a4ea4dbb495d41e5ef93d8ffec352ef05a275167
Naman <namankumar62039@gmail.com>
2024-01-19 00:42:30 +0530

    created cart function in context component

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -3,6 +3,14 @@ import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
+const GetDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < all_product.length; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+}
+
 const ShopContextProvider = (props) => {
   const ContextValue = { all_product };
 
