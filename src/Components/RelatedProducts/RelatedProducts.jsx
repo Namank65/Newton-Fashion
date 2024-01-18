e68146471fcce1a441c879f4567b7858993332cc
Naman <namankumar62039@gmail.com>
2024-01-18 23:46:42 +0530

    completed the discription box css file and fixed bugs

---
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RelatedProducts.css';
+
+const RelatedProducts = () => {
+  return (
+    <div className='RelatedProducts'>
+        
+    </div>
+  )
+}
+
+export default RelatedProducts;
