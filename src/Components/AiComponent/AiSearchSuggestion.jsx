8b7108618867dd2a6b28245f08a901d13c42ff40
Naman <namankumar62039@gmail.com>
2024-03-05 19:14:04 +0530

    making the data visible on aisearchSuggestion componenet

--- src/Components/AiComponent/AiSearchSuggestion.jsx
+++ src/Components/AiComponent/AiSearchSuggestion.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 
 const AiSearchSuggestion = () => {
+
+  const ai = useSelector((store) => store.ai);
+  console.log(ai)
+
+  // const {clothpic, clothNames} = ai;
+
   return (
     <div>AiSearchSuggestion</div>
   )
