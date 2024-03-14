629bb736e389adba85ea0aeb9524172c7fdc28d8
Naman <namankumar62039@gmail.com>
2024-03-14 19:07:47 +0530

    fixed some styling bug

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,2 +1,9 @@
 export const UNSPLACH_KEY = "TXCIS2W2EX2U1uLLGoQrgfOL53sPxvlWDohLir-jW0g";
 export const OPENAI_KEY = "";
+
+
+
+
+
+const number1 = sk-hzUezeayIFJlA8rJSEc4T3Blb     
+  const number2 =    kFJZcv57nHaMzUiY0KqJfwZ
