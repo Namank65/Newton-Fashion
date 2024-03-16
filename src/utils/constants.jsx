3b20afd6bf6901c3330a08c937e05a93758ab4a3
Naman <namankumar62039@gmail.com>
2024-03-17 00:25:29 +0530

    made the app more secure to prevent hacking by adding .env file

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,7 +1,2 @@
-export const UNSPLACH_KEY = "TXCIS2W2EX2U1uLLGoQrgfOL53sPxvlWDohLir-jW0g";
-export const OPENAI_KEY = "";
-
-
-
-
-//sk-hzUezeayIFJlA8rJSEc4T3Blb kFJZcv57nHaMzUiY0KqJfwZ
+export const UNSPLACH_KEY = process.env.REACT_APP_UNSPLACH_KEY;
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
