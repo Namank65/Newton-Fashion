4fdb5989ac6167c8f1574902192ce002503d5b18
Naman <namankumar62039@gmail.com>
2024-03-23 01:19:05 +0530

    made the login page more responsive and user friendly

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
+
+  const [isRegistered, setIsRegistered] = useState(true);
+
+  const RegisterHandelClick = () => {
+    setIsRegistered(!isRegistered);
+  }
+
   return (
     <div className="loginsignup">
-      <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+      <form onSubmit={(e) => e.preventDefault()} className="loginsignup-container">
+        <h1>{isRegistered ? "Login" : "Register"}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder="Your Name" />
-          <input type="email" placeholder="Email Address" />
+          <input type="text" placeholder="User Name" />
+          {!isRegistered && (<input type="email" placeholder="Email Address" />)}
           <input type="password" placeholder="Password" />
         </div>
         <button>Continue</button>
         <p className="loginsignup-login">
-          Already Have An Account? <span>Login Here</span>
+          {isRegistered ? "New To Nubi Fashion" : "Already Have An Account?"} <span onClick={RegisterHandelClick}>{isRegistered ? "Register" : "Login"}</span>
         </p>
         <div className="loginsignup-agree">
           <input type="checkbox" name="" id="" />
           <p>By Continuing, I Agree To The Terms Of Use & Privacy Policy.</p>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
