944be4d801e4bf7e3e2ca9054818890ec72f393c
Naman <namankumar62039@gmail.com>
2024-07-02 01:34:41 +0530

    desription upgraded

--- src/Components/DiscriptionBox/DiscriptionBox.jsx
+++ src/Components/DiscriptionBox/DiscriptionBox.jsx
@@ -12,7 +12,7 @@ const DiscriptionBox = () => {
         <p>
           An e-commerce website is an online platform that facilitate buying and
           selling of products or services over the internet serves as a virtual
-          marketplace where businesses and individ showcase their products,
+          marketplace where businesses and individ showcase their products in different parts of the world,
           interact with customers, and conduc transactions without the need for
           a physical presence. E-com websites have gained immense popularity due
           to their conveni accessibility, and the global reach they offer to keep the product dynamic.
