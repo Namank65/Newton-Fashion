25611ff650f1bdc39fc24fa026a52f9cc9359890
Naman <namankumar62039@gmail.com>
2024-07-08 20:00:14 +0530

    adding discription again

--- src/Components/DiscriptionBox/DiscriptionBox.jsx
+++ src/Components/DiscriptionBox/DiscriptionBox.jsx
@@ -21,7 +21,7 @@ const DiscriptionBox = () => {
         <p>
           E-commerce websites typically display products or services a detailed
           descriptions, images, prices, and any available vareant of that particular product (e.g., sizes,
-          colors). Each product usually has its own details with relevant
+          colors). Each product usually has its own details with relevant products and information
           information and details as required for the product with accurate Discription.
         </p>
       </div>
