e3d745e99583b0ad8c1d9fae502f04a676d754e7
Naman <namankumar62039@gmail.com>
2024-07-16 00:46:15 +0530

    adding header on login page

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -48,6 +48,7 @@ const LoginSignup = () => {
 
   return (
     <div className="loginsignup">
+      <h1> Welcome to Nubi Fashion</h1>
       <form onSubmit={SubmitHandler} className="loginsignup-container">
         <h1>{isRegistered ? "Login" : "Register"}</h1>
 
